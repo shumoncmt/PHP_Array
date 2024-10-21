f0fa382a37c149aa70bf2de9decab2e0ea052182
shumoncmt <shomoncmt@gmail.com>
2024-10-21 17:52:11 +0600

    aff
    add

--- A1.php
+++ A1.php
@@ -7,6 +7,18 @@ $name3 = "Jime";
 
 $names = array ("jhan", "jane", "jamy");
 
+$name =["hjom", "karim", "Shakil"];
+
 $length = count($names);
 
-echo "Lest iten = ". $names[$length-1];
+echo "Lest item = ". $names[$length-1];
+ count($name);
+ echo PHP_EOL;
+ echo count($name); 
+ echo "\n************************************";
+echo "\n";
+ $fruits = array ("mango", "banana", "apple", "grape", "pineapple", "apple", "jackfruits");
+ $length = count($fruits);
+ echo "Last item = ". $fruits[$length-1];
+ echo PHP_EOL;
+echo $length;
---
+++ A2.php
@@ -0,0 +1,11 @@
+<?php
+$fruits =["mango", "grape", "banana", "orange", "pineapple"];
+$length =count($fruits);
+for ($i = 0; $i < $length; $i++) {
+    echo $fruits[$i];
+    echo PHP_EOL;
+
+}
+$name ="Shakil Mia";
+
+print_r($name);
