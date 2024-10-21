2f93ea2e6e49a469c64016b631cad667114edd80
shumoncmt <shomoncmt@gmail.com>
2024-10-21 10:37:56 +0600

    a

--- A1.php
+++ A1.php
@@ -1,3 +1,11 @@
 <php
 
-$fruits = array ();
+$name1 = "Jhon";
+$name2 = "Jene";
+$name3 = "Jime";
+
+$names = array ("jhan", "jane", "jamy");
+
+$length = count($names);
+
+echo "Lest iten = ". $names[$length-1];
