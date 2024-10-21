825eaaeaebb073df47d822057502ac3ef586c8af
shumoncmt <shomoncmt@gmail.com>
2024-10-21 18:08:16 +0600

    ADD

---
+++ A3.php
@@ -0,0 +1,17 @@
+<?php
+$fruits = ["mango", "grape", "apple"];
+print_r($fruits);
+
+// $fruits [] = "Banana";
+// $fruits [] = "Pineapple";
+
+array_push($fruits, "banana");
+// $last_item =array_pop($fruits);
+// array_pop($fruits);
+// array_pop($fruits);
+
+
+print_r($fruits);
+// print_r($fruits);
+// print_r($fruits);
+//echo $last_item;
