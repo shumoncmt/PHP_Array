32df30ab5b4fbcf748694700148ad276cde78d4f
shumoncmt <shomoncmt@gmail.com>
2024-10-22 12:32:18 +0600

    add
    add

---
+++ A5.php
@@ -0,0 +1,8 @@
+<?php
+$fruits =[
+    "mango",
+    "grape",
+    "apple"
+];
+$fruits [0]= "pineapple"; //array value replace
+print_r($fruits);
---
+++ A6.php
@@ -0,0 +1,8 @@
+<?php
+$fruits = [
+    "mango"=> "আম",
+     "grape"=> "আঙ্গুর", 
+    "apple"=> "আপেল"
+];//associative array key as value
+$fruits ["banana"] = "কলা";
+print_r($fruits);
