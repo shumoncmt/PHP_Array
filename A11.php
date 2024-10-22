fd0b569fd7df74867b753711106a4fe4809a3027
shumoncmt <shomoncmt@gmail.com>
2024-10-22 17:54:05 +0600

    add
    ad

--- A11.php
+++ A11.php
@@ -7,4 +7,6 @@ $all_fruits = array_merge($fruits, $nre_fruits);
 print_r($all_fruits);
 
 $class3 = ["A", "B", "C"];
-$class4 = ["E", ]
+$class4 = ["E", "D", "Z", "E", "H", "A", "I" ];
+$common_item= array_intersect($class3, $class4);
+print_r($common_item);
