98a7353c8a20da767f82495d4e6bdae93ba69722
shumoncmt <shomoncmt@gmail.com>
2024-10-23 10:56:52 +0600

    add
    dd

--- string/string.php
+++ string/string.php
@@ -1,4 +1,14 @@
 <?php
-$string = ("Hello Bangladesh, This is my fist tasting");
-echo $string ;
-echo str_repeat("",2) . " " ."fist tasting";
+$string = ("Hello Bangladesh, This is my fist tasting This is my fist tasting");
+//echo $string ;
+// echo str_repeat("",2) . " " ."fist tasting"; //Repeat String
+// echo str_replace("Bangladesh", "Dhaka", $string); //Replace String
+// echo PHP_EOL;
+// echo str_shuffle("Jamal");
+// echo PHP_EOL;
+// echo str_word_count($string);
+// echo str_ends_with($string,"");
+// echo str_pad($string,0," ", STR_PAD_LEFT);
+// echo str_split($string);
+// echo strlen($string);
+echo str_
