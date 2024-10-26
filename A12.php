ba2b127dd6b5a92b8e9702d0555c7bdc9f80bc45
Md. Shumon Mia <shomoncmt@gmail.com>
2024-10-26 21:02:34 +0600

    add
    add

---
+++ A12.php
@@ -0,0 +1,4 @@
+<?php
+$names = ["Apple", "Bell", "Cat", "Dog","Egg", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+$unique_names = array_unique($names);
+print_r($unique_names);
